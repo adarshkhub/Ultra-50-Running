2766331244e98930a82eaeda962295d4ab470cb3
adarshkhub <67844710+adarshkhub@users.noreply.github.com>
2024-05-08 19:26:56 -0400

    Add files via upload

--- U50running analysis queries.sql
+++ U50running analysis queries.sql
@@ -13,4 +13,19 @@ max(age) as oldest
 From u50running
 group by gender
 
+-- What was the average time for each age group
+-- Need to group in buckets
+
+-- Doesn't Work
+Select total_minutes from u50running
+case when age < 30 then 'age_20to29'
+	 when age < 40 then 'age_30to39'
+	 when age < 50 then 'age_40to49'
+	 when age < 60 then 'age_50to59'
+	 else 'age_60+'
+	 end as age_group
+)
+
+-- Top 3 Males and Females
+
 --What was the average time for each age group (By Decade)
